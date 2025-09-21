a97d81c39edf5617ad1d662c61e18ea55341b2dc
sonpa2002 <119403398+sonpa2002@users.noreply.github.com>
2025-09-21 15:43:39 +0700

    fixtime

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let hls;
 let player;
 let TileVideo;
 
-const updateTime = new Date(2025, 8, 21, 14, 2); // Lưu ý: tháng 0-11 => 7 = tháng 8
+const updateTime = new Date(2025, 8, 21, 15, 45); // Lưu ý: tháng 0-11 => 7 = tháng 8
 // Thời gian hiện tại
 const now = new Date();
 // Tính số phút chênh lệch
