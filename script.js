6d073e8b3c22807d28f79e1f342ca29166c1c2f6
sonpa2002 <119403398+sonpa2002@users.noreply.github.com>
2025-10-09 14:10:37 +0700

    Tap25hayDeToiToaSang

--- script.js
+++ script.js
@@ -39,9 +39,9 @@ function canUserWatch(storedStr, title) {
   const storedUser = parts[0];
   const range = parts[1];
   
-  // if(storedUser==="user102"){
-  //   localStorage.setItem("tokenStaff", "user102+18-22");
-  // }
+  if(storedUser==="user101"){
+    localStorage.setItem("tokenStaff", "user101+01-33");
+  }
 
 
   // 3. Tách start-end
