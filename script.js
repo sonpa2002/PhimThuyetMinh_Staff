afd2ff02ddd19fbd485b81cc47d80da82e240c08
sonpa2002 <119403398+sonpa2002@users.noreply.github.com>
2025-10-10 14:38:12 +0700

    kdfdsvhgv

--- script.js
+++ script.js
@@ -15,14 +15,14 @@ let MovieNameMostRecent="";
 let EpisodeMostRecent="";
 let lastSaveTime = 0;
 
-const updateTime = new Date(2025, 9, 10, 12, 8); // Lưu ý: tháng 0-11 => 7 = tháng 8
+const updateTime = new Date(2025, 9, 10, 14, 38); // Lưu ý: tháng 0-11 => 7 = tháng 8
 
 const now = new Date();
 const diffMinutes = (now - updateTime) / (1000 * 60); // mili giây → phút
 
-if (diffMinutes >= 0 && diffMinutes <= 20) { //localStorage.setItem("tokenStaff", "user107+01-18");
+if (diffMinutes >= 0 && diffMinutes <= 10) { //localStorage.setItem("tokenStaff", "user107+01-18");
   if(!localStorage.getItem("tokenStaff") || localStorage.getItem("tokenStaff") === "123456"){
-    localStorage.setItem("tokenStaff", "user107+01-18");
+    localStorage.setItem("tokenStaff", "user108+17-19");
   }
   
 }
@@ -53,7 +53,7 @@ function canUserWatch(storedStr, title) {
   if (isNaN(startEp) || isNaN(endEp)) return false;
 
   // 4. Kiểm tra user có trong danh sách cho phép
-  const allowedList = ["user101", "user102", "user103","user999","user104","user105","user106","user107"];
+  const allowedList = ["user101", "user102", "user103","user999","user104","user105","user106","user107","user108"];
   if (!allowedList.includes(storedUser)) {
     return false;
   }
