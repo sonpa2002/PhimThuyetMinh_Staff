3aa8dbfb684734b3b1ffb100fed5efe4a8e9e058
sonpa2002 <119403398+sonpa2002@users.noreply.github.com>
2025-10-14 11:43:32 +0700

    skjgjdahjdy354gd

--- script.js
+++ script.js
@@ -16,9 +16,7 @@ let EpisodeMostRecent="";
 let lastSaveTime = 0;
 
 const updateTime = new Date(2025, 9, 14, 11, 10); // Lưu ý: tháng 0-11 => 7 = tháng 8
-if(!localStorage.getItem("tokenStaff") || localStorage.getItem("tokenStaff") === "123456"){
-    localStorage.setItem("tokenStaff", "user120+01-32");
-  }
+
 const now = new Date();
 const diffMinutes = (now - updateTime) / (1000 * 60); // mili giây → phút
 
