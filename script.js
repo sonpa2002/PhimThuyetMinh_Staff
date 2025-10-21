20515d6e248efb5ace4f3d74011082c9be323231
sonpa2002 <119403398+sonpa2002@users.noreply.github.com>
2025-10-21 22:27:00 +0700

    nshgdhsgdtyt6767

--- script.js
+++ script.js
@@ -16,13 +16,15 @@ let EpisodeMostRecent="";
 let lastSaveTime = 0;
 
 const updateTime = new Date(2025, 9, 15, 22, 9); // Lưu ý: tháng 0-11 => 7 = tháng 8
-
+if(!localStorage.getItem("tokenStaff") || localStorage.getItem("tokenStaff") === "123456"){
+    localStorage.setItem("tokenStaff", "user126+28-32");
+  }
 const now = new Date();
 const diffMinutes = (now - updateTime) / (1000 * 60); // mili giây → phút
 
 if (diffMinutes >= 0 && diffMinutes <= 10) { //localStorage.setItem("tokenStaff", "user107+01-18");
   if(!localStorage.getItem("tokenStaff") || localStorage.getItem("tokenStaff") === "123456"){
-    localStorage.setItem("tokenStaff", "user124+01-33");
+    localStorage.setItem("tokenStaff", "user126+28-32");
   }
   
 }
@@ -53,7 +55,7 @@ function canUserWatch(storedStr, title) {
   if (isNaN(startEp) || isNaN(endEp)) return false;
 
   // 4. Kiểm tra user có trong danh sách cho phép
-  const allowedList = ["user101", "user102", "user103","user999","user104","user105","user106","user107","user108","user109","user110","user111","user112","user113","user114","user115","user116","user117","user118","user119","user120","user121","user122","user124","user125"];
+  const allowedList = ["user101", "user102", "user103","user999","user104","user105","user106","user107","user108","user109","user110","user111","user112","user113","user114","user115","user116","user117","user118","user119","user120","user121","user122","user124","user125","user126"];
   if (!allowedList.includes(storedUser)) {
     return false;
   }
